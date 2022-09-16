7f0063046e6ac9485ee298acb6a266c60a87b278
Paul Osinowo <paulosinowo@Pauls-MacBook-Pro.local>
2022-09-16 14:59:59 +0100

    add logout btn

--- src/pages/TaskView.tsx
+++ src/pages/TaskView.tsx
@@ -1,13 +1,26 @@
-import React from "react";
+import React, {useContext} from "react";
+import { storageService } from "../services/storageService";
+import { authCtx } from ".";
 import "../App.scss";
 
 const TaskView: React.FC<{}> = () => {
+  const context = useContext(authCtx);
+  const { setAuthenticated } = context;
+
+  const logout = () => {
+    setAuthenticated(null)
+    storageService.removeTokens()
+  }
+
   return (
     <>
+    <div>
+      <button className="temp-logout-btn" onClick={logout} >Log Out</button>
       <div className="App">
         <div className="App-logo">
           <p>🧪</p>
         </div>
+      </div>
       </div>
     </>
   );
